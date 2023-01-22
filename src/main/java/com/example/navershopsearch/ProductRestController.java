5b0ce68dc22aedff53511d4e72f2158abc0161fc
sumin <sumincathy@naver.com>
2023-01-23 00:19:39 +0900

    신규상품등록 기능 추가

--- src/main/java/com/example/navershopsearch/ProductRestController.java
+++ src/main/java/com/example/navershopsearch/ProductRestController.java
@@ -2,6 +2,8 @@ package com.example.navershopsearch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,11 +12,20 @@ import java.util.List;
 @RestController // JSON으로 데이터를 주고받음을 선언
 public class ProductRestController {
 
+    private final ProductService productService;
     private final ProductRepository productRepository;
 
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    // 신규 상품 등록
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        productRepository.save(product);
+        return product;
     }
 }
