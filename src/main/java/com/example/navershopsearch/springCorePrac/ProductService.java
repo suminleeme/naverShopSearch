586416051b727ea8f10305bab420c87ba3b1462a
sumin <sumincathy@naver.com>
2023-01-26 22:57:49 +0900

    코드 리팩토링

---
+++ src/main/java/com/example/navershopsearch/springCorePrac/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.navershopsearch.springCorePrac;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService() {
+        ProductRepository productRepository = new ProductRepository();
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(ProductRequestDto requestDto) throws SQLException {
+        // 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto);
+
+        productRepository.createProduct(product);
+
+        return product;
+    }
+
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
+        Product product = productRepository.getProduct(id);
+
+        if (product == null) {
+            throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
+        }
+        int myprice = requestDto.getMyprice();
+        productRepository.updateMyprice(id, myprice);
+        return product;
+    }
+
+    public List<Product> getProducts() throws SQLException {
+        List<Product> products = productRepository.getAllProducts();
+
+        return products;
+    }
+}
