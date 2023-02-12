85333824afb3ad43fcaa69eabce9939d7758dd6e
sumin <sumincathy@naver.com>
2023-02-12 23:46:27 +0900

    로그인 완료 처리

--- src/main/java/com/example/navershopsearch/security/SecurityConfiguration.java
+++ src/main/java/com/example/navershopsearch/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.navershopsearch.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,11 +21,13 @@ public class SecurityConfiguration {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
         return (web) -> web.ignoring().antMatchers("/h2-console/**");
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 회원 관리 처리 aAPI (POST /user/**)에 대해 CSRF 무시
         http.csrf()
                 .ignoringAntMatchers("/user/**");
 
@@ -40,13 +43,20 @@ public class SecurityConfiguration {
                 .and()
                 // 로그인 기능
                 .formLogin()
+                // 로그인 View 제공 (GET /user/login)
                 .loginPage("/user/login")
+                // 로그인 처리 (POST /user/login)
+                .loginProcessingUrl("/user/login")
+                // 로그인 처리 후 성공 시 URL
                 .defaultSuccessUrl("/")
+                // 로그인 처리 후 실패 시 URL
                 .failureUrl("/user/login?error")
                 .permitAll()
                 .and()
                 // 로그아웃 기능
                 .logout()
+                // 로그아웃 처리 Url
+                .logoutUrl("/user/logout")
                 .permitAll();
         return http.build();
     }
