48f279d29f92094874d79ce31d73994014e2951f
sumin <sumincathy@naver.com>
2023-02-13 00:00:45 +0900

    로그인시 로그인id 상단에 표시, 로그아웃 링크 추가

---
+++ src/main/java/com/example/navershopsearch/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.navershopsearch.controller;
+
+import com.example.navershopsearch.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    //model 에 'username' 전달
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
+        return "index";
+    }
+}
